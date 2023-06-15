2a4a3462a7ee6033a9983250a45b387957504449
Ojas Singh <ojas.singh02@gmail.com>
2023-06-15 17:00:26 -0400

    bug fixes

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -4,7 +4,7 @@ import { UilLinkedin, UilGithub, UilEnvelopeAlt} from '@iconscout/react-unicons'
 const Footer = () => {
     return(<div className="bg-dark py-10 px-40  grid place-content-center">
         {/* <h1 className="text-white">Hello</h1> */}
-        <div className="flex text-center text-gray-500 xs:ml-4 scale-150 py-3 ml-18">
+        <div className="flex text-center text-gray-500 scale-150 py-3">
             <motion.a target="_blank" href='https://www.linkedin.com/in/ojas-singh/' whileHover={{translateY: -4}} className='mr-1 text-center'><UilLinkedin/></motion.a>
             <motion.a target="_blank" href='https://github.com/ojassingh/' whileHover={{translateY: -4}} className='mr-1'><UilGithub/></motion.a>
             <motion.a target="_blank" href='mailto:ojas.singh02@gmail.com' whileHover={{translateY: -4}} className='mr-1'><UilEnvelopeAlt/></motion.a>
