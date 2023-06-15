37623d8bb33050462d0af5c59ea277e2ac2a4532
Ojas Singh <ojas.singh02@gmail.com>
2023-06-15 17:03:51 -0400

    braekpoint fixes

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -2,9 +2,9 @@ import { motion } from "framer-motion";
 import { UilLinkedin, UilGithub, UilEnvelopeAlt} from '@iconscout/react-unicons'
 
 const Footer = () => {
-    return(<div className="bg-dark py-10 md:px-40 lg:px-40 px-20 sm:px-30  grid place-content-center">
+    return(<div className="bg-dark py-10 md:px-40 lg:px-40 px-20 sm:px-30  grid place-content-center mx-auto">
         {/* <h1 className="text-white">Hello</h1> */}
-        <div className="flex text-center text-gray-500 scale-150 py-3">
+        <div className="flex text-center text-gray-500 scale-150 py-3 mx-auto">
             <motion.a target="_blank" href='https://www.linkedin.com/in/ojas-singh/' whileHover={{translateY: -4}} className='mr-1 text-center'><UilLinkedin/></motion.a>
             <motion.a target="_blank" href='https://github.com/ojassingh/' whileHover={{translateY: -4}} className='mr-1'><UilGithub/></motion.a>
             <motion.a target="_blank" href='mailto:ojas.singh02@gmail.com' whileHover={{translateY: -4}} className='mr-1'><UilEnvelopeAlt/></motion.a>
