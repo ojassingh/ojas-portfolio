7615539b317e292eb8d874140c479bbc64a2e66f
ojassingh <ojassingh@Ojass-MacBook-Pro-2.local>
2023-07-07 18:55:04 -0400

    Updated FavIcon

--- pages/index.tsx
+++ pages/index.tsx
@@ -5,10 +5,18 @@ import Intro from "../components/Intro";
 import Experience from "../components/Experience/Experience";
 import Footer from "../components/Footer";
 import Front from "../components/Front";
+import Head from "next/head";
+import { HandWaving } from "@phosphor-icons/react";
 
 const Home: NextPage = () => {
   return (
     <div className="">
+      <Head>
+        <title>Ojas Singh</title>
+        <meta name="description" content="Ojas Singh's Portfolio" />
+        <link rel="icon" href="/hand-waving.png"/>
+      </Head>
+
        <div className="hidden md:block lg:block">
         <Navbar/>
         </div>
